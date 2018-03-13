b162985746eba6e4cadcd94cda94547aeeab8ba6
Lucas LI <scofieldgood@gmail.com>
2018-03-13 15:51:36 +0800

    added component 'new-problem' and 'navbar'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,16 @@ import { ProblemListComponent } from './components/problem-list/problem-list.com
 import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
 //2:15:41
 import { DataService } from "./services/data.service";
+import { NewProblemComponent } from './components/new-problem/new-problem.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProblemListComponent,
-    ProblemDetailComponent
+    ProblemDetailComponent,
+    NewProblemComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,15 +6,22 @@ import { PROBLEMS } from "../mock-problems";
 @Injectable()
 export class DataService {
 
+  problems: Problem[] = PROBLEMS; // init from PROBLEMS at first
+
   constructor() { }
 
   getProblems(): Problem[] {
-    return PROBLEMS;
+    return this.problems;
   }
 
   getProblem(id: number): Problem {
-    return PROBLEMS.find(
+    return this.problems.find(
       (problem) => problem.id == id
     );
+  }
+
+  addProblem(problem: Problem): void {
+    problem.id = this.problems.length + 1;
+    this.problems.push(problem);
   }
 }
