640204fc7607597c87e3f6bfa6c9cf53b5d52bee
Lucas LI <scofieldgood@gmail.com>
2018-03-17 16:01:59 +0800

    enable language selection

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -13,13 +13,27 @@ export class EditorComponent implements OnInit {
 
   editor: any;
 
+  public languages: string[] = ['Java', 'C++', 'Python'];
+  language: string = 'Java';
+
   // default solution content
   defaultContent = {
     'Java': `public class Solution {
     public static void main(String[] args) {
       // write your code here
     }
-}`
+}`,
+    'C++': `#include <iostream>;
+using namespace std;
+
+int main() {
+  // wrtie your code here
+  return 0;
+}`,
+    'Python': `class Solution:
+  def solution():
+    # write your code here
+  `
   }
 
   constructor() {
@@ -28,8 +42,23 @@ export class EditorComponent implements OnInit {
   ngOnInit() {
     this.editor = ace.edit('editor');
     this.editor.setTheme('ace/theme/eclipse');
-    this.editor.getSession().setMode('ace/mode/java');
-    this.editor.setValue(this.defaultContent["Java"]);
+    this.resetEditor();
+    this.editor.$blockScrolling = Infinity;
   }
 
+  setLanguage(language: string): void {
+    this.language = language;
+    console.log('reseting editor');
+    this.resetEditor();
+  }
+
+  resetEditor(): void {
+    this.editor.getSession().setMode('ace/mode/' + this.language.toLowerCase());
+    this.editor.setValue(this.defaultContent[this.language]);
+  }
+
+  submit(): void {
+    let userCode = this.editor.getValue();
+    console.log(userCode);
+  }
 }
